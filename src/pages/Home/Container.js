762ba71cc145bd7012df2eab8cc12deb8f3fbf9b
dongYoun2 <ehddbs6425@naver.com>
2020-12-13 17:03:00 +0900

    Home container

--- src/pages/Home/Container.js
+++ src/pages/Home/Container.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Presenter from './Presenter';
+import { movieApi } from 'api';
 
 class Container extends React.Component {
   state = {
     nowPlaying: null,
-    upComing: null,
+    upoming: null,
     popular: null,
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      const { data: { results: nowPlaying } } = await movieApi.getNowPlaying();
+      const { data: { results: upcoming } } = await movieApi.getUpcoming();
+      const { data: { results: popular } } = await movieApi.getPopular();
+
+      this.setState({ nowPlaying, upcoming, popular });
+    } catch {
+      this.setState({ error: "Can't find movies information." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { nowPlaying, upComing, popular, error, loading } = this.state;
