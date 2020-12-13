342e065aa3e9a7e4cb24b9e44f7c97062873fa00
dongYoun2 <ehddbs6425@naver.com>
2020-12-13 17:22:47 +0900

    Search container

--- src/pages/Search/Container.js
+++ src/pages/Search/Container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Presenter from './Presenter';
+import { movieApi, tvApi } from 'api';
 
 class Container extends React.Component {
   state = {
@@ -8,6 +9,29 @@ class Container extends React.Component {
     searchTerm: '',
     loading: false,
     error: null,
+  };
+
+  handleSubmit = () => {
+    if(this.state.searchTerm !== '') {
+      this.search();
+    }
+  };
+
+  search = async () => {
+    const { searchTerm } = this.state;
+
+    this.setState({ loading: true });
+
+    try {
+      const { data: { results: movies } } = await movieApi.searchByTerm(searchTerm);
+      const { data: { results: tvs } } = await tvApi.searchByTerm(searchTerm);
+
+      this.setState({ movies, tvs });
+    } catch {
+      this.setState({ error: "Can't find any results" });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -20,6 +44,7 @@ class Container extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
