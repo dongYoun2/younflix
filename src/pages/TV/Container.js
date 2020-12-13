119b29fca266180be4aa0df62ff8a784b5152173
dongYoun2 <ehddbs6425@naver.com>
2020-12-13 17:07:52 +0900

    TV container

--- src/pages/TV/Container.js
+++ src/pages/TV/Container.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import Presenter from './Presenter';
+import { tvApi } from 'api';
 
 class Container extends React.Component {
   state = {
     topRated: null,
     popular: null,
-    airingTody: null,
+    airingToday: null,
     loading: true,
     error: null,
   };
 
+  async componentDidMount() {
+    try {
+      const { data: { results: topRated } } = await tvApi.getTopRated();
+      const { data: { results: popular } } = await tvApi.getPopular();
+      const { data: { results: airingToday } } = await tvApi.getAiringToday();
+
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({ error: "Can't find TVs information." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    const { topRated, popular, airingTody, loading, error } = this.state;
+    const { topRated, popular, airingToday, loading, error } = this.state;
 
     return (
       <Presenter 
         topRated={topRated}
         popular={popular}
-        airingTody={airingTody}
+        airingToday={airingToday}
         loading={loading}
         error={error}
       />
