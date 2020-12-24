c74e6a2d7fa83237cfba9e966c4d853b249c5135
dongYoun2 <ehddbs6425@naver.com>
2020-12-25 01:47:28 +0900

    Section, MovieList Presenter, Loader

--- src/pages/MovieList/Presenter.js
+++ src/pages/MovieList/Presenter.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from 'components/Loader';
+import Section from 'components/Section';
 
+const Container = styled.div`
+  padding: 0 30px;
+`
 function MovieListPresenter({ nowPlaying, upcoming, popular, error, isLoading }) {
+  if(isLoading) {
+    return <Loader />;
+  }
+
+  if(error) {
+    return <h3>Something went wrong. Page not found.</h3>;
+  }
+
+  // no error === nowPlaying, upcoming, popular all are array
+
   return (
-    <div>Movie</div>
+    <Container>
+      <Section title="Now Playing">
+        {nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}
+      </Section>
+      <Section title="Upcoming Movies">
+        {upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}
+      </Section>
+      <Section title="Popular Movies">
+        {popular.map(movie => <span key={movie.id}>{movie.title}</span>)}
+      </Section>
+    </Container>
   );
 }
 
